96140b6a90ba2a5288e0f5abe3192839ebae2baf
24andahalf <dave.rigley@gmail.com>
2019-03-12 15:19:14 -0300

    bigMap.html, Fullmap.jpg, and POI.js added
    Adding base elements for this starter

---
+++ POI.js
@@ -0,0 +1,24 @@
+var POIobjs = [
+			{
+				POIid: 		"A0",
+				hoverText: 	"Snarf!",
+				popupText: 	"Here's some more text for your stinkin eyeballs ya buncha weirdos!", 
+				x:			500,
+				y:			400
+
+			}, 
+			{
+				POIid: 		"A1",		
+				hoverText: 	"Barf!",
+				popupText: 	"Here's some more stinkin text for your stinkin eyeballs ya stinkin buncha stinkin weirdos!",
+				x:			300,
+				y:			800
+			}, 
+			{
+				POIid: 		"A2",		
+				hoverText: 	"Baasfdasfarf!",
+				popupText: 	"Here's someasfasfasfas more stinkin text for your stinkin eyeballs ya stinkin buncha stinkin weirdos!",
+				x:			350,
+				y:			850
+			}
+		]
