17f8a26364ebcbf4496c2cef90c27c92c24f3986
24andahalf <dave.rigley@gmail.com>
2019-03-14 13:39:45 -0300

    Add files via upload
    Added images to popup header, started adding in content from Ger for POIs

--- POI.js
+++ POI.js
@@ -1,24 +1,49 @@
 var POIobjs = [
 			{
-				POIid: 		"A0",
-				hoverText: 	"Snarf!",
-				popupText: 	"Here's some more text for your stinkin eyeballs ya buncha weirdos!", 
-				x:			500,
-				y:			400
+				POIid: 				"A0",
+				hoverText: 			"In the beginning...",
+				popupText: 			"Formed a Party. Spent the night in jail. Took a shower. Bought groceries.", 
+				popupHeaderImage:	"open-book",
+				x:					500,
+				y:					400
 
 			}, 
 			{
-				POIid: 		"A1",		
-				hoverText: 	"Barf!",
-				popupText: 	"Here's some more stinkin text for your stinkin eyeballs ya stinkin buncha stinkin weirdos!",
-				x:			300,
-				y:			800
+				POIid: 				"A1",		
+				hoverText: 			"FIGHT - 4 Bandits",
+				popupText: 			"Punch punch fight fight! We won! Be more descriptive in here.",
+				popupHeaderImage:	"crossed-swords",
+				x:					300,
+				y:					800
 			}, 
 			{
-				POIid: 		"A2",		
-				hoverText: 	"Baasfdasfarf!",
-				popupText: 	"Here's someasfasfasfas more stinkin text for your stinkin eyeballs ya stinkin buncha stinkin weirdos!",
-				x:			350,
-				y:			850
+				POIid: 				"A2",		
+				hoverText: 			"FIGHT - Keiji and his crew",
+				popupText: 			"Defeated a band of....er.... bandits. Rescued Tasi.",
+				popupHeaderImage:	"crossed-swords",
+				x:					350,
+				y:					850
+			}, 
+			{
+				POIid: 				"A3",		
+				hoverText: 			"Found Mysteline",
+				popupText: 			"Delivered Paspa's package and sent on a fetch quest.",
+				popupHeaderImage:	"propeller-beanie",
+				x:					450,
+				y:					850
 			}
-		]
+	
+		// Add any new POI Objects in here please :)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		];
