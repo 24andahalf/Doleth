61abf4a27d49ad72c8bc4fc10a97d399bb729ba8
24andahalf <dave.rigley@gmail.com>
2021-06-02 18:46:21 -0300

    Update ntPOI.js
    another try

--- scripts/js/ntPOI.js
+++ scripts/js/ntPOI.js
@@ -4,8 +4,8 @@ var ntPOIobjs = [
 				hoverText: "The caravan's destination",
 				popupText: "Bix and Parn are hoping to trade their silver and iron ore as well as steel for the ironroot wood harvested in this area.",
 				popupHeaderImage: "robe",
-				x: 1785.4500122070312,
-				y: 598.9062347412109
+				x: 1785,
+				y: 598
 				
 
 			}
