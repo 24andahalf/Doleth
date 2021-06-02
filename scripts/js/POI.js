49588206521c35d5e4ddb534849cb0a6e5945085
24andahalf <dave.rigley@gmail.com>
2021-06-02 18:35:49 -0300

    Update POI.js
    Adding proper descriptions for current locations as well as reference to one of the non-timeline locations.

--- scripts/js/POI.js
+++ scripts/js/POI.js
@@ -3,8 +3,8 @@ var POIobjs = [
 			{
 			POIid: "A000",
 			hoverText: "Adventure awaits...",
-			popupText: "GUZZAH!!!!",
-			popupHeaderImage: "robe",
+			popupText: "The heroes are hired by Bix Silvershine and his brother _____ to help them transport _________ to <button class='gotoAndSee' data-gotoID='B000'>destination</button>",
+			popupHeaderImage: "mimic",
 			x: 425,
 			y: 788  
 			}
@@ -13,7 +13,7 @@ var POIobjs = [
 			{
 			POIid: "A001",
 			hoverText: "Rest stop",
-			popupText: "1 day intop travels",
+			popupText: "After a day's travel the group makes camp at a location next to the road that seems safe. Partway into the night the caravan is ambushed by bandits. The heroes spring into action eliminating a number of the bandits but several run off into the woods.",
 			popupHeaderImage: "robe",
 			x: 560,
 			y: 759
@@ -23,7 +23,7 @@ var POIobjs = [
 			{
 			POIid: "A002",
 			hoverText: "Cave in the woods",
-			popupText: "",
+			popupText: "The heroes track down the escaping bandits, eliminating 2 and capturing one for later interrogation(or turning them in for a reward). During the showdown a cave is discovered, but what is inside?",
 			popupHeaderImage: "robe",
 			x: 531,
 			y: 731
